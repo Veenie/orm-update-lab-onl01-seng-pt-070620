ca0983131ea1fbc24ff8fea7eb03c3a7dd2942fe
Raveen K Sharma <raveen08@gmail.com>
2020-09-09 23:50:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,15 @@ attr_reader :id
     DB[:conn].execute(sql)
   end
  
-  
+  def save
+  sql = <<-SQL
+    INSERT INTO songs (name, album)
+    VALUES (?, ?)
+  SQL
+ 
+  DB[:conn].execute(sql, self.name, self.album)
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end
  
   def self.create(name:, grade:)
     song = Song.new(name, grade)
